cce273883e07c096debfb21fb7e28538ab678528
Jamal <jamal@DESKTOP-36B518Q>
2024-03-11 23:05:43 -0500

    push up from prac

--- employee.js
+++ employee.js
@@ -1,1 +1,23 @@
-// Your code here 
+// Your code here
+class Employee {
+    constructor(name,occupation){
+        this.name = name
+        this.occupation = occupation;
+    }
+
+    sayName() {
+        console.log(`${this.name} says hello`)
+    }
+
+    sayOccupation(){
+        console.log(`${this.name} is a ${this.occupation}`)
+    }
+
+
+}
+
+
+let johnWick = new Employee("John Wick","Dog Lover")
+johnWick.sayName()
+
+module.exports = Employee;
--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-// Your code here 
+// Your code here
+const Employee = require('./employee')
+
+let johnWick = new Employee("John Wick","Dog Lover")
+
+let boundJohn = johnWick.sayName.bind(johnWick)
+
+setTimeout(boundJohn,2000)
+
+johnWick.sayName()
+
+let occ = johnWick.sayOccupation.bind(johnWick)
+
+setTimeout(occ,3000)
